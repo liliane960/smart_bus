75f032c81adcc8b664be2cadf0791f59836d8fab
liliane960 <ishimwelily12@gmail.com>
2025-07-13 03:52:26 +0200

    update police

---
+++ police/police_dashboard.php
@@ -0,0 +1,34 @@
+<?php require_once "../db.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Smart Bus Dashboard police</title>
+<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="dashboard">
+    <aside class="sidebar">
+        <h2>Smart Bus</h2>
+        <ul>
+            <li><a href="#" data-page="view_logs.php" class="active">View Logs</a></li>
+            <li><a href="#" data-page="view_plate_number.php">View Plate Number</a></li>
+            <li><a href="#" data-page="view_notifications.php">View Notifications</a></li>
+        </ul>
+    </aside>
+    <div class="main">
+        <header class="header">
+            <h1>Dashboard</h1>
+            <div>
+                <a href="logout.php" class="logout">Logout</a>
+            </div>
+            <div id="datetime"></div>
+        </header>
+        <main id="main-content">
+            <?php include 'view_logs.php'; ?>
+        </main>
+    </div>
+</div>
+<script src="assets/script.js"></script>
+</body>
+</html>
